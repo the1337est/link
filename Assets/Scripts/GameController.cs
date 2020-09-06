cdeab4d3aab104387f428a923d2bd6e4ba4c0569
Gaurav Tripathi <gauravtripathi198@gmail.com>
2020-09-05 20:01:19 -0400

    Added Folder structure and prefabs

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController Instance;
+
+    [SerializeField] private Texture2D levelMap;
+    [SerializeField] private GameObject tile;
+
+    public List<Tile> Tiles;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                DestroyImmediate(this);
+            }
+        }
+    }
+
+    public void CreateLevel()
+    {
+        if (levelMap != null)
+        {
+
+            //for(int x = 0; x < levelMap.wid)
+        }
+    }
+    
+}
